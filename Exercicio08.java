bf0ea755daa818e6283824362097f91ad70238a9
Samuel Odecio <108842146+SamuelOdecio@users.noreply.github.com>
2022-08-01 19:34:17 -0400

    Update Exercicio08.java

--- Exercicio08.java
+++ Exercicio08.java
@@ -9,6 +9,31 @@ média das pessoas com menos de 10 anos.*/
 public class Exercicio08 {
 
     public static void main(String[] args) {
+        String nomes[] = new String[5];
+        int idade[] = new int[5];
+        int altura[] = new int[5];
+        int peso[] = new int[5];
+        String aux;
+        int contador, auxiliar;
+        for (int i = 0; i <= 20; i++) {
+            nomes[i] = JOptionPane.showInputDialog("PESSOA " + (i + 1) + " DIGITE SEU NOME: ");
+            aux = JOptionPane.showInputDialog("PESSOA " + (i + 1) + " DIGITE SEU PESO: ");
+            peso[i] = Integer.parseInt(aux);
+            aux = JOptionPane.showInputDialog("PESSOA " + (i + 1) + " DIGITE SUA IDADE: ");
+            idade[i] = Integer.parseInt(aux);
+            aux = JOptionPane.showInputDialog("PESSOA " + (i + 1) + " DIGITE SUA ALTURA:  (em CM)");
+            altura[i] = Integer.parseInt(aux);
 
+        }
+        auxiliar = idade[0];
+        for (int i = 1; i <= 20; i++) {
+            if (auxiliar < idade[i]) {
+                auxiliar = idade[i];
+                contador = i;
+                
+            }
+        }
+        
+        JOptionPane.showMessageDialog(null, "O Nome da Pessoa mais velha é: "  );
     }
 }
